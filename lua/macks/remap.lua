cc387d8c3dc4982e8c08b43269a2ace04dee5e19
= <=>
2025-05-26 11:10:43 -0400

    Better pasting keybinds

--- lua/macks/remap.lua
+++ lua/macks/remap.lua
@@ -61,3 +61,13 @@ end)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- special paste to void
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+
+vim.keymap.set({'n','v'}, '<C-c>', '"+y')
+vim.keymap.set({'n','v'}, '<C-v>', '"+p')
