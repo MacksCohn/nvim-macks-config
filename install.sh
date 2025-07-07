c60866cfa5ecc687cc42d47e403f217563cd9c42
AnimatorMax <maxcohn@live.com>
2025-07-07 10:03:49 -0400

    Made installs all in one script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+gh release download --repo neovim/neovim  --pattern nvim-linux-x86_64.tar.gz
+sudo mkdir /opt/nvim/
+sudo tar xzvf nvim-linux-x86_64.tar.gz -C /opt/nvim
+sudo ln -s /opt/nvim/nvim-linux-x86_64/bin/nvim /usr/bin/nvim
+rm nvim-linux-x86_64.tar.gz
+cp nvim ~/.config
