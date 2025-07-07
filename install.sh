486e6b83dc651171d0b64fbab5dcfd4c2c89150e
AnimatorMax <maxcohn@live.com>
2025-07-07 10:11:08 -0400

    Created uninstall script and improved install

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo mkdir /opt/nvim/
 sudo tar xzvf nvim-linux-x86_64.tar.gz -C /opt/nvim
 sudo ln -s /opt/nvim/nvim-linux-x86_64/bin/nvim /usr/bin/nvim
 rm nvim-linux-x86_64.tar.gz
-cp nvim ~/.config
+cp -r nvim ~/.config
